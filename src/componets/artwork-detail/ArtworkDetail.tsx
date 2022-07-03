3af5af5b8bc5b50ccc1c9790ef1e579f3156e583
Paola <paomedina_88@hotmail.com>
2022-07-02 21:45:47 -0300

    minors improvements at modal

--- src/componets/artwork-detail/ArtworkDetail.tsx
+++ src/componets/artwork-detail/ArtworkDetail.tsx
@@ -1,29 +1,12 @@
-import { Tablet } from '@mui/icons-material';
+
 import { autocompleteClasses, Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, CircularProgress, Collapse, Dialog, Grid, IconButton, IconButtonProps, ListItemSecondaryAction, Modal, Paper, styled, Typography } from '@mui/material';
 import React, { useState } from 'react'
 import useFetchArtworkById from '../../hooks/useFetchArtworkById';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { palette } from '@mui/system';
+import CloseIcon from '@mui/icons-material/Close';
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  borderRadius: '10px',
-  p: 4,
-  width: '900px',
-  height: '1000px'
-};
-
-const styleModal = {
-  width: 'auto',
-  height: '1000px',
-}
 
 
 const styleScroll = {
@@ -59,45 +42,49 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   }),
 }));
 
-
-
 type Props = {
   artworkId: number;
   onModalClose: () => void;
 }
 
 export const ArtworkDetail = ({ artworkId, onModalClose }: Props) => {
+
   const [openModal, setOpenModal] = useState(true);
+  const { artwork, isLoading } = useFetchArtworkById(artworkId);
+  const [expanded, setExpanded] = React.useState(false);
 
   const handleModalClose = () => {
     setOpenModal(false);
     onModalClose();
   }
 
-  const { artwork, isLoading } = useFetchArtworkById(artworkId);
-
-  const [expanded, setExpanded] = React.useState(false);
-
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  return (
-    <Modal sx={styleModal}
+  return (    
+    <Modal 
       open={openModal}
       onClose={handleModalClose}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
 
-      <Card sx={{ maxWidth: 800, style }}>
+      <Card sx={{ maxWidth: 800, mr: 'auto',ml:'auto', mt:'5px'}}>
         <CardHeader
           avatar={
-            <Avatar aria-label="recipe" src="/broken-image.jpg" sx={{ bgcolor: '#D69D66' }} />
+            <Avatar
+              aria-label="recipe"
+              src="/broken-image.jpg"
+              sx={{ bgcolor: '#D69D66' }}
+            />
           }
           action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
+            <IconButton 
+            aria-label="settings"
+            onClick={handleModalClose}
+            >
+              <CloseIcon/>
             </IconButton>
           }
           title={artwork.artist_title}
@@ -107,21 +94,22 @@ export const ArtworkDetail = ({ artworkId, onModalClose }: Props) => {
           isLoading ? <CircularProgress /> :
             <CardMedia
               component="img"
-              height="650"
-              image={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/800,/0/default.jpg`}
+              height="600"
+              image={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
               alt={"texto alt"}
             />
         }
 
         <CardContent>
-          <Typography 
+          <Typography
             fontFamily={'Montserrat'}
+            fontWeight={'800'}
+            fontSize={'x-large'}
             id="modal-title"
             variant="subtitle1"
             gutterBottom
-            color= 'palette.text.secondary'
-            >
-            
+            color='text.secondary'
+          >
             {artwork.title}
           </Typography>
         </CardContent>
@@ -133,65 +121,44 @@ export const ArtworkDetail = ({ artworkId, onModalClose }: Props) => {
           <IconButton aria-label="share">
             <ShareIcon />
           </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
+          {artwork.publication_history ?
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+            : null
+          }
         </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Collapse
+          in={expanded}
+          timeout="auto"
+          unmountOnExit
+        >
           <CardContent sx={styleScroll}>
-            <Typography paragraph fontFamily={'Montserrat'}>Detalle de la Obra:</Typography>
             <Typography paragraph
+              fontFamily={'Montserrat'}
+              fontWeight={'600'}
+            >
+              Detalle de la Obra:
+            </Typography>
+            <Typography
+              paragraph
               whiteSpace={'break-spaces'}
               fontFamily={'Montserrat'}
+              fontWeight={'600'}
+              lineHeight={'1.2'}
+              fontSize={'initial'}
               color="pallete.text.primary"
-              sx={{ mt: 2, height: 280, overflow: 'scroll', overflowX: 'hidden' }}>
+              sx={{ mt: 2, height: 260, overflow: 'scroll', overflowX: 'hidden' }}>
               {artwork.publication_history}
             </Typography>
           </CardContent>
         </Collapse>
-
-
-
-        {/* <Box sx={style}>
-        {
-          isLoading ? <CircularProgress /> :
-          <img
-          src={
-            `https://www.artic.edu/iiif/2/${artwork.image_id}/full/800,/0/default.jpg`
-          }
-          alt={"texto alt"}
-          width={"800px"}
-          height={"620px"}
-          />
-        } */}
-        {/* <Typography
-          fontFamily={'Montserrat'}
-          id="modal-title"
-          variant="subtitle1"
-          gutterBottom>
-          {artwork.title}
-        </Typography>
-
-
-        <Box component={Paper} sx={styleScroll}>
-
-          <Typography paragraph
-            whiteSpace={'break-spaces'}
-            fontFamily={'Montserrat'}
-            sx={{ mt: 2, height: 280, overflow: 'scroll', overflowX: 'hidden' }}>
-            {artwork.publication_history}
-          </Typography>
-        </Box> */}
-
       </Card>
-
-
-      {/* </Box> */}
     </Modal >
   )
 }
--- src/componets/home/Home.tsx
+++ src/componets/home/Home.tsx
@@ -3,7 +3,7 @@ import './Home.css';
 function Home() {
   return (
     <picture>
-        <img src={process.env.PUBLIC_URL + '/images/bg-gallery.png'} alt="bg-gallery"  width="100%" height="100%"/>
+        <img src={process.env.PUBLIC_URL + '/images/bg-gallery.png'} alt="bg-gallery"  width="100%"/>
     </picture>
   )
 }
